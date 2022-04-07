30b50a63a3ea6083e25a9a6eefacc4ed7c766d56
Manish Kumar <manishhansalmeh@gmail.com>
2022-04-07 16:43:54 +0530

    Checkout Page completed

--- src/Components/Homepage/Entertainment.jsx
+++ src/Components/Homepage/Entertainment.jsx
@@ -54,19 +54,23 @@ export const Entertainment = () => {
     const responsive = {
         superLargeDesktop: {
             breakpoint: { max: 4000, min: 3000 },
-            items: 6
+            items: 6,
+            slidesToSlide: 5
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 5
+            items: 5,
+            slidesToSlide: 5
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
-            items: 2
+            items: 2,
+            slidesToSlide: 2
         },
         mobile: {
             breakpoint: { max: 464, min: 0 },
-            items: 1
+            items: 1,
+            slidesToSlide: 1
         }
     };
     return (
